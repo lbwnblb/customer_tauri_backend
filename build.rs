80886ead572baad01b8ee0051cc89c46b6e715d5
Your Name <you@example.com>
2026-05-20 21:32:41 +0800

    1

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 fn main() {
-    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
-    prost_build::compile_protos(&["proto/dy_im_proto.proto"], &["proto/"]).unwrap();
+    let protoc = protoc_bin_vendored::protoc_bin_path().unwrap();
+
+    std::env::set_var("PROTOC", protoc);  // 告诉 prost-build 用这个 protoc
+
+    prost_build::compile_protos(
+        &["src/proto/dy_im.proto"],
+        &["src/proto/"]
+    ).unwrap();
     tauri_build::build()
 }
